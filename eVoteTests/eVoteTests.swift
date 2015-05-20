069be30849d14a9c4b34dafce61d6ababa900941
Björn Orri Saemundsson <Bjoern@Bjorns-Mac.local>
2015-05-20 10:18:15 +0200

    Initial Commit

---
+++ eVoteTests/eVoteTests.swift
@@ -0,0 +1,36 @@
+//
+//  eVoteTests.swift
+//  eVoteTests
+//
+//  Created by Björn Orri Saemundsson on 5/20/15.
+//  Copyright (c) 2015 Eurescom. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class eVoteTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
